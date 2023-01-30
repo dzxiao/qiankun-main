0240e4ede9029b657619cd48cb9c28934b4be968
dengzixiao <dzxXiao1111@163.com>
2023-01-30 15:20:46 +0800

    feat: configureQK

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
+import { registerMicroApps, start } from 'qiankun';
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+registerMicroApps([
+  {
+    name: 'son-one',
+    entry: '//localhost:81',
+    container: '#son-one',
+    activeRule: '/son-one',
+  },
+]);
+// 启动 qiankun
+start();
 new Vue({
   router,
   store,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>主应用</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+<style>
+.home{
+  background-color: rgb(218, 206, 206);
+}
+</style>
